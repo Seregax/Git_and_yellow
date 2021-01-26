ff489a2933cc9b55236f155a1d3292e19fcc4a0a
Seregax <Serega-x5trf@yandex.ru>
2021-01-26 20:59:52 +0300

    Git и желтые окружности

---
+++ main.py
@@ -0,0 +1,40 @@
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+import sys
+from random import randint
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.pushButton.clicked.connect(self.run)
+        self.paint = False
+
+    def run(self):
+        self.paint = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(event, qp)
+            qp.end()
+
+    def draw(self, event, qp):
+        try:
+            qp.setBrush(QColor(255, 255, 0))
+            a = randint(20, 300)
+            qp.drawEllipse(0, 0, a, a)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
